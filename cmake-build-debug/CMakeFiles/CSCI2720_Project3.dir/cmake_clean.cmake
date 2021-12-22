2cf5ac7ad67849b936770327189fe3f23ccc7499
JohnLavender474 <jml62608@uga.edu>
2021-12-21 23:49:13 -0500

    Reformatted code style.

---
+++ cmake-build-debug/CMakeFiles/CSCI2720_Project3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSCI2720_Project3.dir/Project3/P3Executable.cpp.o"
+  "CSCI2720_Project3"
+  "CSCI2720_Project3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSCI2720_Project3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
